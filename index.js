f735a0d1b5697d2b682066f32091b9b6a5af4db5
Obtez <59846143+Obtez@users.noreply.github.com>
2021-10-21 14:17:07 +0200

    setup the basic html and configured typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+var heading = document.querySelector('h1');
+var mainBtn = document.querySelector('#main-btn');
+mainBtn.addEventListener('click', function () {
+    return null;
+});
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+const heading: HTMLHeadingElement = document.querySelector('h1');
+const mainBtn: HTMLButtonElement = document.querySelector('#main-btn');
+
+mainBtn.addEventListener('click', () => {
+  return null;
+});
